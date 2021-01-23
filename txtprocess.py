3726af6188bbc77100b9995c4699d2fd53748fee
andy1747369004 <1747369004@qq.com>
2021-01-23 20:46:07 +0800

    1231

--- txtprocess.py
+++ txtprocess.py
@@ -20,3 +20,15 @@ for i in range(15):
 
 
  #啊是大飒飒大大
+
+for word in words:
+    if len(word) == 1:
+        continue
+    else:
+        counts[word] = counts.get(word, 0) + 1
+
+items = list(counts.items())
+items.sort(key=lambda x: x[1], reverse=True)
+for i in range(15):
+    word, value = items[i]
+    print('{0:<10}{1:>5}'.format(word, value))
