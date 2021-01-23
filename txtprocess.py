7c47837e7d0106e46339dc4652386ec83f4594cc
andy1747369004 <1747369004@qq.com>
2021-01-23 20:50:13 +0800

    1231

--- txtprocess.py
+++ txtprocess.py
@@ -19,16 +19,3 @@ for i in range(15):
     print('{0:<10}{1:>5}'.format(word,value))
 
 
- #啊是大飒飒大大
-
-for word in words:
-    if len(word) == 1:
-        continue
-    else:
-        counts[word] = counts.get(word, 0) + 1
-
-items = list(counts.items())
-items.sort(key=lambda x: x[1], reverse=True)
-for i in range(15):
-    word, value = items[i]
-    print('{0:<10}{1:>5}'.format(word, value))
